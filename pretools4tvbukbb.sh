562cb627f43f6f7eaccd1bae474ea511d0d36f4e
Yile Wang <yile.wang@utdallas.edu>
2023-01-16 16:09:27 -0600

    update pretools4tvbukbb

--- pretools4tvbukbb.sh
+++ pretools4tvbukbb.sh
@@ -9,49 +9,49 @@
 
 # save time
 preHSAM=/media/yat-lok/BATW/HSAMData
+raw_dir=control_raw_dti
 # get list of the control cases
 dir=$(ls -t --color=never $preHSAM/control | cut -d '.' -f 1)
-dir_rawdti=$(ls -t --color=never $preHSAM/control_raw_dti | cut -d '.' -f 1)
+dir_rawdti=$(ls -t --color=never $preHSAM/raw_dir | cut -d '.' -f 1)
 
 
 ### 1. untar files and move to a new folder
 # the reason for this step is to avoid contaminate the original files. We will do that every step during this process
 
 # get list of raw dti cases
-mkdir $preHSAM/control_dti_untar
-cd $preHSAM/control_dti_untar
+mkdir $preHSAM/dti_untar
 
 # use a for loop to untar all the tar.gz file, then rename them to their file names.
 for i in $dir_rawdti
 do
-    tar -C . -xzf $preHSAM/control_raw_dti/${i}.tar.gz
-    mv $preHSAM/control_dti_untar/raw $preHSAM/control_dti_untar/$i
+    tar -C $preHSAM/dti_untar -xzf $preHSAM/$raw_dir/${i}.tar.gz
+    mv $preHSAM/dti_untar/raw $preHSAM/dti_untar/$i
 done
 
 ### 2. convert .PAR .REC files to nii file using `dcm2niix` tool for DTI data
 
 # create a folder to storge the niigz files
-mkdir $preHSAM/control_dti_niigz
+mkdir $preHSAM/dti_niigz
 for i in $dir_rawdti
 do
-	cp $preHSAM/control_dti_untar/$i/*.PAR.gz $preHSAM/control_dti_niigz/$i
-	cp $preHSAM/control_dti_untar/$i/*.REC.gz $preHSAM/control_dti_niigz/$i
-	gzip -d $preHSAM/control_dti_niigz/$i/*
-	dcm2niix -f %p_%s $preHSAM/control_dti_niigz/$i
+	cp $preHSAM/dti_untar/$i/*.PAR.gz $preHSAM/dti_niigz/$i
+	cp $preHSAM/dti_untar/$i/*.REC.gz $preHSAM/dti_niigz/$i
+	gzip -d $preHSAM/dti_niigz/$i/*
+	dcm2niix -f %p_%s $preHSAM/dti_niigz/$i
     # clean the redundancy
-	rm $preHSAM/control_dti_niigz/$i/*.PAR*
-	rm $preHSAM/control_dti_niigz/$i/*.REC*
+	rm $preHSAM/dti_niigz/$i/*.PAR*
+	rm $preHSAM/dti_niigz/$i/*.REC*
 done
 
 ### 3. DTI and struct+fMRI matching
 
 # a dir to put DTI and struct+fMRI
-mkdir $preHSAM/control_final
+mkdir $preHSAM/final
 # define a function to help us matching
 matching () {
 	if [ $1 == $2 ];
 	then
-		cp $preHSAM/control_dti_niigz/$3/*DTI* $preHSAM/control_final/$1
+		cp $preHSAM/dti_niigz/$3/*DTI* $preHSAM/final/$1
 		echo "matching done"
 	fi
 }
@@ -78,16 +78,16 @@ done
 
 for i in $dir
 do
-	gzip -d $preHSAM/control_final/$i/struct+orig.*
-	gzip -d $preHSAM/control_final/$i/raw_restinglores_long1+orig.*
+	gzip -d $preHSAM/final/$i/struct+orig.*
+	gzip -d $preHSAM/final/$i/raw_restinglores_long1+orig.*
 	# using 3dAFNItoNIFTI rename the file to what `tvb-ukbb` requires
-	3dAFNItoNIFTI -prefix "$i"_T1 $preHSAM/control_final/$i/struct+orig.
-	3dAFNItoNIFTI -prefix "$i"_FMRI_RESTING $preHSAM/control_final/$i/raw_restinglores_long1+orig.
+	3dAFNItoNIFTI -prefix "$i"_T1 $preHSAM/final/$i/struct+orig.
+	3dAFNItoNIFTI -prefix "$i"_FMRI_RESTING $preHSAM/final/$i/raw_restinglores_long1+orig.
 	mv "$i"_* $i
-	rm $preHSAM/control_final/$i/raw_restinglores*
-	rm $preHSAM/control_final/$i/struct+orig.*
+	rm $preHSAM/final/$i/raw_restinglores*
+	rm $preHSAM/final/$i/struct+orig.*
     # compress them to nii.gz 
-    gzip $preHSAM/control_final/$i/*.nii
+    gzip $preHSAM/final/$i/*.nii
 	echo "$i" is done	
 done
 
@@ -95,11 +95,11 @@ done
 ### 5. rename DTI files
 # the reason why we have this step is because the matching process. We cannot rename the DTI file before the matching finishes. It's a special condition due to the data itself.
 
-mkdir $preHSAM/control_final2
-
+mkdir $preHSAM/final2
+prefix=$preHSAM/final2
 for i in $dir
 do
-	file=$(ls -t $preHSAM/control_final2/$i/*DTI* | head -n 1)
+	file=$(ls -t $prefix/$i/*DTI* | head -n 1)
 	if [ -f "$file" ];
 	then
 		echo "$i" have DTI files
